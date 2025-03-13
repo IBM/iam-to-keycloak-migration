cea0cedfa14473f2b91852b9b7d2472a69c164f6
Vlad Balanescu <vlad_balanescu@yahoo.com>
2025-03-13 08:36:46 +0000

    Update iam-keycloak-migration.sh
    Co-authored-by: James Hewitt <james.hewitt@gmail.com>
    Signed-off-by: Vlad Balanescu <vlad.balanescu@uk.ibm.com>

--- iam-keycloak-migration.sh
+++ iam-keycloak-migration.sh
@@ -567,7 +567,7 @@ then
   # Check the integration internal IKU is reconciled and if so, use t instead of the CS one
   integrationIKUPhase="$(oc get integrationkeycloakuser "$keycloakIntegrationUserName" -n "$servicesNamespace" -o jsonpath='{.status.phase}' --ignore-not-found=true)"
   if [[ "$integrationIKUPhase" == "reconciled" ]]; then
-    secretToFetch=$keycloakIntegrationAdminSecretName
+    secretToFetch="$keycloakIntegrationAdminSecretName"
     keycloakAdminUser="$keycloakIntegrationUserName"
   fi  
   keycloakAdminPass="$(oc get secret "$secretToFetch" -n "$servicesNamespace" -o jsonpath="{.data.password}" | base64 -d)"
